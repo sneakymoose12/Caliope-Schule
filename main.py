8277d3d313c420310c9dc78f579a7b1eea071c20
sneakymoose12 <aaronjos17juli@gmail.com>
2025-04-01 11:17:39 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,6 @@
+basic.show_string("Hello World!")
+music.play_melody("C5 B A G F E D C ", 120)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("Hello World!")
+    } else {
+        basic.showString("drück Knopf A")
+    }
+})
