7b5c2948fd13c2c37b0ee8ba0114e5ffa5ddbe6b
sneakymoose12 <aaronjos17juli@gmail.com>
2025-04-01 11:22:38 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Timer = 5
     for (let index = 0; index < 5; index++) {
         basic.pause(1000)
-        Timer += 1
+        Timer += -1
         basic.showString("" + (Timer))
     }
+    music.playMelody("A F E F D G E F ", 120)
 })
